50db7ae7ac52ad859c4a06c1b7cc655285cca933
xuwushun <1471132931@qq.com>
2016-10-11 19:52:50 +0800

    commit

---
+++ src/main/java/com/microlecture/MicrolectureApplication.java
@@ -0,0 +1,12 @@
+package com.microlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrolectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrolectureApplication.class, args);
+	}
+}
