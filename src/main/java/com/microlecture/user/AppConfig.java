ed7f716ea1425226547aeb4fed4b5295c6eb1112
xuwushun <1471132931@qq.com>
2016-10-12 14:09:10 +0800

    config appconfig

--- src/main/java/com/microlecture/user/AppConfig.java
+++ src/main/java/com/microlecture/user/AppConfig.java
@@ -1,7 +1,12 @@
 package com.microlecture.user;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by xuwushun on 2016/10/12.
  */
+@Configuration
+@ComponentScan(basePackages = "com.microlecture")
 public class AppConfig {
 }
