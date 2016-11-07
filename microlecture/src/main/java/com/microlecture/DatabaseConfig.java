33762c6f543573887b4f3e5e1dae4a6511642ad1
xuwushun <1471132931@qq.com>
2016-11-07 23:07:50 +0800

    add database sync

---
+++ microlecture/src/main/java/com/microlecture/DatabaseConfig.java
@@ -0,0 +1,14 @@
+package com.microlecture;
+
+/**
+ * Created by xuwushun on 2016/11/7.
+ */
+public class DatabaseConfig {
+	private static final String url = "jdbc:mysql:localhost/mysql";
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String user = "root";
+	private static final String password = "root";
+	public static void main(String[] args){
+		
+	}
+}
